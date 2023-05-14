2c49d4918770edd452fbbcc08a137763a78d060a
Irfan Q <ibma1u21@soton.ac.uk>
2023-05-14 05:11:02 +0100

    Added ball

--- breakout.py
+++ breakout.py
@@ -7,36 +7,79 @@ display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, pen_type=PEN_P4, rotate=0)
 display.set_backlight(0.5)
 display.set_font("bitmap8")
 
+WIDTH, HEIGHT = display.get_bounds()
+
+BG = display.create_pen(40, 40, 40)
 WHITE = display.create_pen(255, 255, 255)
-BLACK = display.create_pen(0, 0, 0)
+RED = display.create_pen(255, 0, 0)
+BLUE = display.create_pen(255, 0, 0)
 
 btn_a = Button(12)
 btn_b = Button(13)
 btn_x = Button(14)
 btn_y = Button(15)
 
-WIDTH, HEIGHT = display.get_bounds()
-
 cols = 6
 rows = 6
 
-BG = display.create_pen(40, 40, 40)
-
 class Paddle():
     def __init__(self):
-        self.x = int(WIDTH/2)
-        self.y = HEIGHT-10
-        self.width = 50
+        self.width = 60
         self.height = 5
-        self.speed = 10
+        self.x = WIDTH/2 - (self.width / 2)
+        self.y = HEIGHT-10
+        self.speed = 25
         self.colour = WHITE
 
+class Ball():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.radius = 5
+        self.x_velocity = 0
+        self.y_velocity = 0
+        self.colour = RED  
+
+class Block():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 60
+        self.height = 30
+        self.points = 1
+        self.colour = BLUE
+
+def renderPaddle(obj):
+    display.set_pen(obj.colour)
+    display.rectangle(int(obj.x), int(obj.y), obj.width, obj.height)
+
+def renderBall(obj):
+    display.set_pen(obj.colour)
+    display.circle(int(obj.x), int(obj.y), obj.radius)
+    obj.x += obj.x_velocity
+    obj.y += obj.y_velocity
+
 lol = Paddle()
+ball = Ball(lol.x + (lol.width / 2), lol.y - 15)
 
-display.set_pen(lol.colour)
-display.rectangle(lol.x, lol.y, lol.width, lol.height)
-display.update()
+blocks = []
+blocks.append(Block(5,0))
+blocks.append(Block(70,0))
+blocks.append(Block(135,0))
 
+for block in blocks:
+    display.rectangle(block.x, block.y, block.width, block.height)
+
+while True:
+    renderPaddle(lol)
+    display.update()
+    renderBall(ball)
+    if btn_a.read():
+        ball.y_velocity = -2
+        ball.x_velocity = 0.5
+        break
+
+# Game Loop
 while True:
     display.set_pen(BG)
     display.clear()
@@ -47,8 +90,22 @@ while True:
     if btn_b.read():
         lol.x -= lol.speed
     
-    display.set_pen(lol.colour)
-    display.rectangle(lol.x, lol.y, lol.width, lol.height)
+    xmax = WIDTH - ball.radius
+    xmin = ball.radius
+    ymax = HEIGHT - ball.radius
+    ymin = ball.radius
+
+    if ball.x < xmin or ball.x > xmax:
+        ball.x_velocity *= -1
+
+    if ball.y < ymin:
+        ball.y_velocity *= -1
+    
+    if (ball.y >= lol.y-lol.height) and (lol.x <= ball.x <= lol.x + lol.width):
+        ball.y_velocity *= -1
+    
+    renderPaddle(lol)
+    renderBall(ball)
     display.update()
     time.sleep(0.01)
     
