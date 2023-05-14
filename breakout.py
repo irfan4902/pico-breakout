a0d554a5a9afa33c651d0b30af3025dd2dc8ce13
Irfan Q <ibma1u21@soton.ac.uk>
2023-05-14 05:49:19 +0100

    Added blocks

--- breakout.py
+++ breakout.py
@@ -12,15 +12,12 @@ WIDTH, HEIGHT = display.get_bounds()
 BG = display.create_pen(40, 40, 40)
 WHITE = display.create_pen(255, 255, 255)
 RED = display.create_pen(255, 0, 0)
-BLUE = display.create_pen(255, 0, 0)
+BLUE = display.create_pen(0, 0, 255)
 
 btn_a = Button(12)
 btn_b = Button(13)
 btn_x = Button(14)
 btn_y = Button(15)
-
-cols = 6
-rows = 6
 
 class Paddle():
     def __init__(self):
@@ -44,11 +41,13 @@ class Block():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = 60
-        self.height = 30
+        self.width = 30
+        self.height = 15
         self.points = 1
         self.colour = BLUE
+        self.destroyed = False
 
+# Render functions
 def renderPaddle(obj):
     display.set_pen(obj.colour)
     display.rectangle(int(obj.x), int(obj.y), obj.width, obj.height)
@@ -59,27 +58,68 @@ def renderBall(obj):
     obj.x += obj.x_velocity
     obj.y += obj.y_velocity
 
+def renderBlocks():
+    for block in blocks:
+        if (not block.destroyed):
+            display.set_pen(block.colour)
+            display.rectangle(block.x, block.y, block.width, block.height)
+        else:
+            display.set_pen(BG)
+            display.rectangle(block.x, block.y, block.width, block.height)
+
+# Create ball and paddle
 lol = Paddle()
 ball = Ball(lol.x + (lol.width / 2), lol.y - 15)
 
+# Create all the blocks
 blocks = []
-blocks.append(Block(5,0))
-blocks.append(Block(70,0))
-blocks.append(Block(135,0))
+blocks.append(Block(15,5))
+blocks.append(Block(50,5))
+blocks.append(Block(85,5))
+blocks.append(Block(120,5))
+blocks.append(Block(155,5))
+blocks.append(Block(190,5))
 
-for block in blocks:
-    display.rectangle(block.x, block.y, block.width, block.height)
+blocks.append(Block(15,25))
+blocks.append(Block(50,25))
+blocks.append(Block(85,25))
+blocks.append(Block(120,25))
+blocks.append(Block(155,25))
+blocks.append(Block(190,25))
 
+blocks.append(Block(15,45))
+blocks.append(Block(50,45))
+blocks.append(Block(85,45))
+blocks.append(Block(120,45))
+blocks.append(Block(155,45))
+blocks.append(Block(190,45))
+
+# Game loop before the game starts
 while True:
+    display.set_pen(BG)
+    display.clear()
+    
+    if btn_y.read():
+        lol.x += lol.speed
+    
+    if btn_b.read():
+        lol.x -= lol.speed
+    
+    ball.x = lol.x + (lol.width / 2)
+    
     renderPaddle(lol)
-    display.update()
     renderBall(ball)
+    renderBlocks()
+    
     if btn_a.read():
         ball.y_velocity = -2
         ball.x_velocity = 0.5
         break
+    
+    display.update()
+    time.sleep(0.01)
 
-# Game Loop
+# Game loop when the game starts
 while True:
     display.set_pen(BG)
     display.clear()
@@ -104,6 +144,13 @@ while True:
     if (ball.y >= lol.y-lol.height) and (lol.x <= ball.x <= lol.x + lol.width):
         ball.y_velocity *= -1
     
+    for block in blocks:
+        if (block.x <= ball.x <= block.x+block.width) and (block.y <= ball.y <= block.y+block.height) and (not block.destroyed):
+            block.destroyed = True
+            ball.y_velocity *= -1
+            ball.x_velocity *= -1
+    
+    renderBlocks()
     renderPaddle(lol)
     renderBall(ball)
     display.update()
